16fe8ac1af33ae04ea826a4572b5eedfd4f15bcb
mathis marschutz <mathis.marschutz@epfedu.fr>
2025-12-17 15:18:43 +0100

    ajout de la classe des cellules allumees

---
+++ src/mini_projet2/cellule_allume.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mini_projet2;
+
+/**
+ *
+ * @author mmars
+ */
+public class cellule_allume {
+   private boolean etat;
+
+    /**
+     * Constructeur par défaut.
+     * Initialise la cellule à l'état éteint.
+     */
+    public cellule_allume() {
+        etat = false;
+    }
+
+    /**
+     * Inverse l'état de la cellule.
+     */
+    public void activercellule() {
+        etat = !etat;
+    }
+
+    /**
+     * Éteint la cellule.
+     */
+    public void eteindrecellule() {
+        etat = false;
+    }
+
+    /**
+     * Vérifie si la cellule est éteinte.
+     * @return true si éteinte, false sinon
+     */
+    public boolean esteteint() {
+        return !etat;
+    }
+
+    /**
+     * Retourne l'état de la cellule.
+     * @return true si allumée, false si éteinte
+     */
+    public boolean getetat() {
+        return etat;
+    }
+
+    /**
+     * Représentation textuelle de la cellule.
+     * @return "X" si allumée, "O" si éteinte
+     */
+    @Override
+    public String toString() {
+        return etat ? "X" : "O";
+    }
+}  
+
