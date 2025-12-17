76ec94eab8ea3f6039b8418022d1a415cb809aab
mathis marschutz <mathis.marschutz@epfedu.fr>
2025-12-17 15:23:00 +0100

    creation grille de jeu

---
+++ src/mini_projet2/grille_de_jeu.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mini_projet2;
+
+/**
+ *
+ * @author mmars
+ */
+public class grille_de_jeu {
+    
+    private cellule_allume[][] matriceCellules;
+    private int nbLignes;
+    private int nbColonnes;
+
+    public grille_de_jeu(int p_nbLignes, int p_nbColonnes) {
+        nbLignes = p_nbLignes;
+        nbColonnes = p_nbColonnes;
+        matriceCellules = new cellule_allume[nbLignes][nbColonnes];
+
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j] = new cellule_allume();
+            }
+        }
+    }
+}
+ 
